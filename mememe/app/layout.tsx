712ae02976f1d0ffe61205dfafe17c5c2c1094ee
Julio M Cruz <julio.cruz@eb-ms.net>
2025-03-01 09:17:31 -0700

    add okto_web3

--- mememe/app/layout.tsx
+++ mememe/app/layout.tsx
@@ -1,24 +1,35 @@
+"use client";
+
 import type React from "react"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import AppProvider from "@/components/providers";
+
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata: Metadata = {
-  title: "Memecoins in your pocket",
-  description: "The easiest way to buy, sell and discover memecoins",
-    generator: 'v0.dev'
-}
 
-export default function RootLayout({
+// export const metadata: Metadata = {
+//   title: "Okto React SDK with Google Auth",
+//   description: "Next.js app integrated with Okto SDK and Google Authentication",
+// };
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-white`}>{children}</body>
+      <body className={`${inter.className} bg-white`}>
+        <AppProvider session={session}>
+          {children}
+        </AppProvider>
+      </body>
     </html>
   )
 }
--- mememe/app/page.tsx
+++ mememe/app/page.tsx
@@ -1,12 +1,55 @@
+"use client";
+
 import Image from "next/image"
 import Link from "next/link"
 import localFont from 'next/font/local'
+
+import React, { useEffect, useMemo } from "react";
+import { useSession, signOut } from "next-auth/react";
+import { LoginButton } from "../components/LoginButton";
+import GetButton from "../components/GetButton";
+import {getAccount, useOkto } from '@okto_web3/react-sdk';
 
 const comicSans = localFont({ 
   src: '../public/fonts/ComicNeue-Regular.ttf',
 })
 
 export default function Home() {
+
+  const { data: session } = useSession();
+  const oktoClient = useOkto();
+
+  //@ts-ignore
+  const idToken = useMemo(() => (session ? session.id_token : null), [session]);
+
+  async function handleAuthenticate(): Promise<any> {
+      if (!idToken) {
+          return { result: false, error: "No google login" };
+      }
+      const user = await oktoClient.loginUsingOAuth({
+          idToken: idToken,
+          provider: 'google',
+      });
+      console.log("Authentication Success", user);
+      return JSON.stringify(user);
+  }
+
+  async function handleLogout() {
+      try {
+          signOut();
+          return { result: "logout success" };
+      } catch (error:any) {
+          return { result: "logout failed" };
+      }
+  }
+
+  useEffect(()=>{
+      if(idToken){
+          handleAuthenticate();
+      }
+  }, [idToken])
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className={`w-full max-w-md mx-auto flex flex-col items-center px-4 ${comicSans.className}`}>
@@ -46,6 +89,14 @@ export default function Home() {
                 className="w-auto h-auto"
               />
             </Link>
+
+
+            <div className="grid grid-cols-2 gap-4 w-full max-w-lg mt-8">
+                <LoginButton />
+                <GetButton title="Okto Log out" apiFn={handleLogout} />
+                <GetButton title="getAccount" apiFn={getAccount} />
+            </div>
+
           </div>
 
           {/* Footer text */}
